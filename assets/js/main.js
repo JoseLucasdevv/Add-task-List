fba0a61aa88b130b44b14a450dda34c2df7b65dd
José Lucas <joselucas.dev@gmail.com>
2022-12-26 12:08:25 -0300

    First-commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const inputTask = document.querySelector(".input-task");
+const btnAddTask = document.querySelector(".btn-add-task");
+const tasks = document.querySelector(".tasks");
+
+btnAddTask.addEventListener("click", (e) => {
+  createTask();
+  inputTask.value = "";
+  inputTask.focus();
+});
+
+inputTask.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    if (!inputTask.value) return;
+    createTask();
+    inputTask.value = "";
+    inputTask.focus();
+  }
+});
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  if (el.classList.contains("apagar")) {
+    el.parentElement.remove();
+    saveTask();
+    inputTask.focus();
+  }
+});
+
+function createLi() {
+  const li = document.createElement("li");
+  return li;
+}
+
+function createTask() {
+  if (!inputTask.value) return;
+  let li = createLi();
+  let btnClean = createCleanBtn();
+  btnClean.innerText = "Clean";
+  btnClean.setAttribute("class", "apagar");
+  btnClean.cl;
+  li.innerHTML = inputTask.value;
+  tasks.appendChild(li);
+  li.appendChild(btnClean);
+  saveTask();
+}
+
+function createCleanBtn() {
+  const btn = document.createElement("button");
+  return btn;
+}
+
+function saveTask() {
+  const task = tasks.querySelectorAll("li");
+  const vectorTask = [];
+  for (let tasks of task) {
+    vectorTask.push(tasks.innerText.replace("Apagar", "").trim());
+  }
+  let jsonTask = JSON.stringify(vectorTask);
+  localStorage.setItem("task", jsonTask);
+}
