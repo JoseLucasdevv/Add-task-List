8250f671562bdd8f2b29d027df07b2b9500235aa
JoseLucasdevv <joselucas.dev@gmail.com>
2023-05-02 10:37:31 -0300

    add new feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 const inputTask = document.querySelector(".input-task");
 const btnAddTask = document.querySelector(".btn-add-task");
 const tasks = document.querySelector(".tasks");
+const container = document.querySelector(".p");
 
 btnAddTask.addEventListener("click", (e) => {
   const inputValue = inputTask.value;
-  console.log(inputValue);
   createTask(inputValue);
   inputTask.value = "";
   inputTask.focus();
@@ -15,10 +15,25 @@ inputTask.addEventListener("keypress", (e) => {
   if (e.keyCode === 13) {
     if (!inputTask.value) return;
     createTask(inputValue);
-    inputTask.value = " ";
+    inputTask.value = "";
     inputTask.focus();
   }
 });
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  if (el.classList.contains("Done")) {
+    if (el.parentElement.style.textDecoration === "none") {
+      el.parentElement.style.textDecoration = "line-through";
+      el.parentElement.style.color = "red";
+    } else {
+      el.parentElement.style.textDecoration = "none";
+      el.parentElement.style.color = "initial";
+    }
+
+    inputTask.focus();
+  }
+});
+
 document.addEventListener("click", (e) => {
   const el = e.target;
   if (el.classList.contains("clean")) {
@@ -32,20 +47,45 @@ function createLi() {
   const li = document.createElement("li");
   return li;
 }
+function createSpan() {
+  const span = document.createElement("span");
+  return span;
+}
 
 function createTask(inputValue) {
+  const spanExist = container.querySelector(".errorLength");
+  if (spanExist) {
+    container.removeChild(container.lastElementChild);
+  }
   if (!inputValue) return;
+  if (inputValue.length > 50) {
+    const span = createSpan();
+    span.innerText = "minimo de 50 caracteres";
+    span.setAttribute("class", "errorLength");
+    container.appendChild(span);
+    return;
+  }
+  if (inputValue.startsWith(" ")) return;
+
   li = createLi();
-  let btnClean = createCleanBtn();
-  btnClean.innerText = "Clean";
+  let btnClean = createBtn();
+  let btnValid = createBtn();
+
+  btnClean.innerHTML = "Clean";
   btnClean.setAttribute("class", "clean");
-  li.innerHTML = inputValue;
+  btnValid.innerHTML = "Done";
+  btnValid.setAttribute("class", "Done");
+  li.setAttribute("class", "styleLi");
+  li.style.textDecoration = "none";
+  li.innerText = inputValue;
   tasks.appendChild(li);
+
+  li.appendChild(btnValid);
   li.appendChild(btnClean);
   saveTask();
 }
 
-function createCleanBtn() {
+function createBtn() {
   const btn = document.createElement("button");
   return btn;
 }
@@ -54,7 +94,7 @@ function saveTask() {
   const task = tasks.querySelectorAll("li");
   const vectorTask = [];
   for (let tasks of task) {
-    vectorTask.push(tasks.innerText.replace("Clean", "").trim());
+    vectorTask.push(tasks.innerText.replace("DoneClean", "").trim());
   }
   const jsonTask = JSON.stringify(vectorTask);
   localStorage.setItem("task", jsonTask);
